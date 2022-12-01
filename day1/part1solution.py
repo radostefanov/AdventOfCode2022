798232d28f63a06e4efe2d642eaacee64398274f
Rado Stefanov <rado.stephan@auxnederlandbv.nl>
2022-12-01 09:16:55 +0100

    day1 print only final result

--- day1/part1solution.py
+++ day1/part1solution.py
@@ -7,4 +7,4 @@ res = functools.reduce(
     lambda acc, el: [0, acc[0] if acc[0] > acc[1] else acc[1]] if el == '\n' else [acc[0] + int(el), acc[1]], lines,
     [0, 0])
 
-print(res)
+print(res[1])
