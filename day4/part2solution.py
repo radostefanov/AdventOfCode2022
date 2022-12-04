0206765a995e9d6779ca113668214cbe7e8f68a3
Rado Stefanov <rado.stephan@auxnederlandbv.nl>
2022-12-04 11:07:29 +0100

    transform day4 part2 into oneliner

--- day4/part2solution.py
+++ day4/part2solution.py
@@ -1,13 +1,1 @@
-import functools
-
-file = [
-    [list(map(int, [z for z in y.split("-")])) for y in x.split(",")] for x in open("input.txt").read().split("\n")
-]
-
-
-def contains_check(acc, e):
-    overlaps = [i for i in [*range(e[0][0], e[0][1] + 1)] if i in [*range(e[1][0], e[1][1] + 1)]]
-    return acc + 1 if len(overlaps) else acc
-
-
-print(functools.reduce(contains_check, file, 0))
+print(sum([(1 if len([i for i in [*range(e[0][0], e[0][1] + 1)] if i in [*range(e[1][0], e[1][1] + 1)]]) else 0) for e in [[list(map(int, [z for z in y.split("-")])) for y in x.split(",")] for x in open("input.txt").read().split("\n")]]))
